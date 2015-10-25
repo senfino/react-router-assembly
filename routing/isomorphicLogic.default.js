cfa459ef7352ab3d51bec829747341d4830785b3
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2015-10-26 00:02:34 +0100

    Isomorphic logic should be directly useful, not a function that returns logic.

--- routing/isomorphicLogic.default.js
+++ routing/isomorphicLogic.default.js
@@ -2,13 +2,10 @@
  * @Author: Tomasz Niezgoda
  * @Date: 2015-10-13 22:04:02
  * @Last Modified by: Tomasz Niezgoda
- * @Last Modified time: 2015-10-18 17:14:08
+ * @Last Modified time: 2015-10-25 21:23:23
  */
 'use strict';
 
-// Put all initiation logic here, it will be reused on the server and client.
-function Logic(){
-
-}
-
-module.exports = Logic;
+module.exports = {
+  // Put all initiation logic here, it will be reused on the server and client.
+};
