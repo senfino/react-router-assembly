0dcdcfbd4980824616f58c653d6dbacc36f63e5b
Tomasz Niezgoda <tomek.niezgoda@mail.com>
2015-11-30 22:38:58 -0500

    Cleaned up example project.

--- example/components/App.js
+++ example/components/App.js
@@ -2,7 +2,7 @@
  * @Author: Tomasz Niezgoda
  * @Date: 2015-10-11 18:18:22
  * @Last Modified by: Tomasz Niezgoda
- * @Last Modified time: 2015-10-18 23:36:12
+ * @Last Modified time: 2015-11-30 22:30:28
  */
 
 'use strict';
@@ -35,7 +35,9 @@ module.exports = React.createClass({
       null,
       React.createElement('h1', null, 'App'),
       'Try turning off JavaScript and checking that the server renders the website.',
+      React.createElement('br'),
       React.createElement(Router.Link, {to: '/apples'}, 'click me for nested Apples component'),
+      React.createElement('br'),
       React.createElement(Router.Link, {to: '/some-non-existent-path'}, 'click me for separate NotFound component'),
       this.props.children,
       'subpage prop = ' + this.state.subpage
